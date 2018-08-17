f05acc938465c6b06305b92552f04e48b3679e1b
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-17 03:01:37 +0300

    Update za.js

--- za.js
+++ za.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+onst Discord = require('discord.js');
 const client = new Discord.Client();
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
